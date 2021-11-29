156b92162764221f82d1f890e3da570149846f56
Matt Collyer <mcollyer@lcs.on.ca>
2021-11-29 11:30:51 -0500

    Beginning to create Circle radius layout. Using static layout and Textfield

--- StatePropertyWrapperSimpleExample/ContentView.swift
+++ StatePropertyWrapperSimpleExample/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("Radius")
+                .bold()
+            TextField("Radius",
+                      text: .constant(""),
+                      prompt:  Text("e.g. : 24.5"))
+        }
+        .navigationTitle("Circle")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView()   
     }
 }
